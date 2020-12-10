cbfe3659c07e6678ac9cd879587edd79f3a5808e
cgetc <cgetc502@gmail.com>
2020-12-10 21:52:20 +0900

    東京電力の料金ページをキャプチャ

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ var puppeteer =　require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: '/home/pptruser/Downloads/example.png'});
+  await page.goto('https://www.kenshin.tepco.co.jp/Certification');
+  await page.screenshot({path: '/home/pptruser/Downloads/example.png', fullPage: true});
 
   await browser.close();
 })();
