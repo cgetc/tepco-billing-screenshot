d6b7bf02cd000369d476a352ada0839fc2a0affc
cgetc <1819135+cgetc@users.noreply.github.com>
2021-09-03 01:00:29 +0900

    timeout 60000

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const env = (name) => process.env[name] || '';
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(60000);
 
   const selectValue = (select, value) => {
     for (var i = 0; i < select.options.length; i++ ) {
